293049b138b5ce14142ea1246df0e0c1448548cb
BoomMan <l.o.v.e_y.o.u_3344@qq.com>
2019-08-11 19:53:37 +0800

    update log LoggerFactory.getLogger

--- spring-boot-logback/src/main/java/cn/boommanpro/springbootlogback/SpringBootLogbackApplication.java
+++ spring-boot-logback/src/main/java/cn/boommanpro/springbootlogback/SpringBootLogbackApplication.java
@@ -1,6 +1,7 @@
 package cn.boommanpro.springbootlogback;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
@@ -8,9 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2019/8/11 18:33
  * @created by BoomManPro
  */
-@Slf4j
 @SpringBootApplication
 public class SpringBootLogbackApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringBootLogbackApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootLogbackApplication.class, args);
