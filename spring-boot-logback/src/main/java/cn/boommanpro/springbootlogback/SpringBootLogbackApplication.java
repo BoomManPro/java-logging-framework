000ada2ba3e265e7fba598e8c0c28efc23d20ee4
BoomMan <l.o.v.e_y.o.u_3344@qq.com>
2019-08-11 18:35:19 +0800

    add spring-boot-logback

---
+++ spring-boot-logback/src/main/java/cn/boommanpro/springbootlogback/SpringBootLogbackApplication.java
@@ -0,0 +1,20 @@
+package cn.boommanpro.springbootlogback;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author <a href="mailto:boommanpro@gmail.com">BoomManPro</a>
+ * @date 2019/8/11 18:33
+ * @created by BoomManPro
+ */
+@Slf4j
+@SpringBootApplication
+public class SpringBootLogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLogbackApplication.class, args);
+        log.info("{} Spring Boot项目启动成功 ",SpringBootLogbackApplication.class);
+    }
+
+}
